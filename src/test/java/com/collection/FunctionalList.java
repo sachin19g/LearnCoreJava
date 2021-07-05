c0be6711376af80100dfaea04bd1dcae97a3599d
sachin19g@gmail.com <sachin19g@gmail.com>
2021-07-05 22:11:52 +0530

    Collection update

---
+++ src/test/java/com/collection/FunctionalList.java
@@ -0,0 +1,25 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FunctionalList {
+
+    public static void main(String[] args) {
+
+        List<Integer> integers = new ArrayList<>();
+        integers.add(1);
+        integers.add(2);
+        integers.add(3);
+        integers.add(4);
+
+         integers.stream().forEach(e-> System.out.println(e));
+
+
+
+
+
+    }
+
+
+}
