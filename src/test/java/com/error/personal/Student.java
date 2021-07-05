47b16815e5e991a1931ca10751921f81f994ba81
sachin19g@gmail.com <sachin19g@gmail.com>
2021-07-06 01:54:41 +0530

    Collection update

--- src/test/java/com/error/personal/Student.java
+++ src/test/java/com/error/personal/Student.java
@@ -1,8 +1,18 @@
 package com.error.personal;
 
+
 public class Student extends Personal {
-    Student(int age,String name){
+
+   String college=null;
+
+    Student(int age,String name,String college){
         super.setAge (age);
         super.setName (name);
+        this.college=college;
+    }
+
+    @Override
+    public String toString() {
+        return "  Name is "+super.getName ()+" Age Is:- "+super.getAge ()+"  College Name is:-"+college;
     }
 }
